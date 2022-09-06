a2e10c6ce9bc673c84de8c4eebc2ad0ac3fa5e42
Maxim Ichetovkin <ichetovkin.maxim@gmail.com>
2022-09-06 08:41:02 +0300

    refactor: добавить императивные  обработчики для открытия попапов

--- mesto-react/src/components/App.js
+++ mesto-react/src/components/App.js
@@ -1,4 +1,4 @@
-import logo from '../images/logo.svg';
+
 
 function App() {
   return (
--- mesto-react/src/components/Main.js
+++ mesto-react/src/components/Main.js
@@ -1,14 +1,30 @@
 function Main() {
+
+    function handleEditAvatarClick() {
+        const popupAvatar = document.querySelector('.popup_type_update-avatar')
+        popupAvatar.classList.add('popup_opend')
+    }
+
+    function handleEditProfileClick() {
+        const popupProfile = document.querySelector('.popup_type_edit-profile')
+        popupProfile.classList.add('popup_opend')
+    }
+
+    function handleAddPlaceClick() {
+        const popupAddCard = document.querySelector('.popup_type_add-card')
+        popupAddCard.classList.add('popup_opend')
+    }
+
     return (
         <main>
             <section className="profile">
-                <div className="profile__image"></div>
+                <div className="profile__image" onClick={handleEditAvatarClick}></div>
                 <div className="profile__name-container">
                     <h1 className="profile__name"></h1>
-                    <button className="profile__edit-button" type="button"></button>
+                    <button className="profile__edit-button" type="button" onClick={handleEditProfileClick}></button>
                     <p className="profile__job"></p>
                 </div>
-                <button className="profile__add-button" type="button"></button>
+                <button className="profile__add-button" type="button" onClick={handleAddPlaceClick}></button>
             </section>
             <section>
                 <ul className="gallery"></ul>

9c21380e173d7cc5d3f1a4e9bf927ac5bb3964fb
Maxim Ichetovkin <ichetovkin.maxim@gmail.com>
2022-09-27 21:05:51 +0300

    fix: убрать лишние атрибуты action и noValidate

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,7 +6,7 @@ function PopupWithForm({ name, title, isOpen, onClose, onSubmit, children }) {
         <div className={`popup popup_type_${name} ${isOpen && 'popup_opend'}`}>
             <div className="popup__container">
                 <button className="popup__close-button" type="button" onClick={onClose} />
-                <form action="save" className="form" name={`${name}`} noValidate onSubmit={onSubmit} >
+                <form className="form" name={`${name}`} onSubmit={onSubmit} >
                     <h3 className="form__title">{`${title}`}</h3>
                     {children}
                 </form>

bde44de51fb316d7b5b93292af3391b4c4cef18a
Maxim Ichetovkin <ichetovkin.maxim@gmail.com>
2022-10-13 20:33:00 +0300

    feat: добавить моб. верстку экрана регистрации и входа

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,36 +21,34 @@ export default function Login({ onSubmit }) {
     }
 
     return (
-        <div className="form__container">
-            <form className="form" name="login" onSubmit={handleLogin}>
-                <h3 className="form__title form__title_type_sign">Вход</h3>
-                <input
-                    className="form__input form__input_type_sign"
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    required
-                    minLength="2"
-                    maxLength="30"
-                    value={credentials.email}
-                    onChange={handleChange}
-                />
-                <span className="form__error name-error" />
-                <input
-                    className="form__input form__input_type_sign"
-                    type="password"
-                    name="password"
-                    placeholder="Пароль"
-                    required
-                    value={credentials.password}
-                    onChange={handleChange}
-                />
-                <span className="form__error link-error" />
-                <button
-                    className="form__submit form__submit_type_sign" type="submit">
-                    Войти
-                </button>
-            </form>
-        </div>
+        <form className="form" name="login" onSubmit={handleLogin}>
+            <h3 className="form__title form__title_type_sign">Вход</h3>
+            <input
+                className="form__input form__input_type_sign"
+                type="email"
+                name="email"
+                placeholder="Email"
+                required
+                minLength="2"
+                maxLength="30"
+                value={credentials.email}
+                onChange={handleChange}
+            />
+            <span className="form__error name-error" />
+            <input
+                className="form__input form__input_type_sign"
+                type="password"
+                name="password"
+                placeholder="Пароль"
+                required
+                value={credentials.password}
+                onChange={handleChange}
+            />
+            <span className="form__error link-error" />
+            <button
+                className="form__submit form__submit_type_sign" type="submit">
+                Войти
+            </button>
+        </form>
     )
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -22,38 +22,36 @@ export default function Register({ onSubmit }) {
     }
 
     return (
-        <div className="form__container">
-            <form className="form" name="login" onSubmit={handleRegister}>
-                <h3 className="form__title form__title_type_sign">Регистрация</h3>
-                <input
-                    className="form__input form__input_type_sign"
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    required
-                    minLength="2"
-                    maxLength="30"
-                    value={credentials.email}
-                    onChange={handleChange}
-                />
-                <span className="form__error name-error" />
-                <input
-                    className="form__input form__input_type_sign"
-                    type="password"
-                    name="password"
-                    placeholder="Пароль"
-                    required
-                    value={credentials.link}
-                    onChange={handleChange}
-                />
-                <span className="form__error link-error" />
-                <button
-                    className="form__submit form__submit_type_sign" type="submit">
-                    Зарегистироваться
-                </button>
-                <p className="form__sign-in-text">Уже зарегистрированы? <Link to="/sign-in" className="form__sign-in-link">Войти</Link></p>
+        <form className="form" name="login" onSubmit={handleRegister}>
+            <h3 className="form__title form__title_type_sign">Регистрация</h3>
+            <input
+                className="form__input form__input_type_sign"
+                type="email"
+                name="email"
+                placeholder="Email"
+                required
+                minLength="2"
+                maxLength="30"
+                value={credentials.email}
+                onChange={handleChange}
+            />
+            <span className="form__error name-error" />
+            <input
+                className="form__input form__input_type_sign"
+                type="password"
+                name="password"
+                placeholder="Пароль"
+                required
+                value={credentials.link}
+                onChange={handleChange}
+            />
+            <span className="form__error link-error" />
+            <button
+                className="form__submit form__submit_type_sign" type="submit">
+                Зарегистрироваться
+            </button>
+            <p className="form__sign-in-text">Уже зарегистрированы? <Link to="/sign-in" className="form__sign-in-link">Войти</Link></p>
 
-            </form>
-        </div>
+        </form>
     )
 }
